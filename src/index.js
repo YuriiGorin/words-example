67cd0539eae38fa91e68c111055260c8da0d8748
CW <uragorin@yandex.ru>
2020-03-07 17:47:31 +0300

    добавлены заготовки для основных классов

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Test from "./classes/Test";
+import Task from "./classes/Task";
+import Service from "./classes/Service";
+
+window.addEventListener("load", () => {
+    const app = new Test();
+});
