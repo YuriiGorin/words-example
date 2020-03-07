beb9b680f6db9227564ccab3d9409088932e9998
CW <uragorin@yandex.ru>
2020-03-07 18:04:34 +0300

    реализован класс Service

--- src/classes/Service.js
+++ src/classes/Service.js
@@ -1,3 +1,22 @@
 export default class Service {
+    constructor(apiUrl) {
+        this._url = apiUrl;
+        this._storageKey = 'TEST_STATE'
+    }
 
+    getTasks() {
+        return fetch(this._url).then(res => res.json());
+    }
+
+    getTestState() {
+       const item = localStorage.getItem(this._storageKey);
+       if (item) {
+           return JSON.parse(item);
+       }
+       return null;
+    }
+
+    saveTestState(data) {
+        localStorage.setItem(this._storageKey, JSON.stringify(data));
+    }
 }
