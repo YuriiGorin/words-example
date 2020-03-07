14d78804a6d89ba496361303af929d72ccac2b74
CW <uragorin@yandex.ru>
2020-03-07 20:23:46 +0300

    реализована базовая функциональность класса Test

--- src/classes/Test.js
+++ src/classes/Test.js
@@ -1,3 +1,40 @@
-export default class Test {
+import Service from "./Service";
+import { Task } from "./Task";
+import shuffle from "lodash/shuffle";
 
+export default class Test {
+    constructor() {
+        this._service = new Service("./data.json");
+        this._service.getTasks().then(data => this._createTasks(data));
+        this._tasks = [];
+        this._current = -1;
+    }
+
+    _createTasks(data) {
+        this._tasks = shuffle(data).map(Task.create);
+        this._tasks.forEach(task => {
+           const checkFn = task.checkAnswer;
+           const self = this;
+           task.checkAnswer = function(answer) {
+               let result = checkFn.call(task, answer);
+               if (result) {
+                   // нет ошибки
+               } else {
+                   // есть ошибка
+               }
+               if (task.isCompleted) {
+                   self._current++;
+               }
+               return result;
+           }
+        });
+        this._current = 0;
+    }
+
+    get task() {
+        if (this._current >= 0) {
+            return this._tasks[this._current];
+        }
+        return null;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import Test from "./classes/Test";
-import { Task } from "./classes/Task";
-import Service from "./classes/Service";
 
 window.addEventListener("load", () => {
-    const app = new Test();
+    window.test = new Test();
 });
 
 // const taskData = [
