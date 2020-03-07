b9a1e4f5fb8db2809e1a2d1fd4127677801e0752
CW <uragorin@yandex.ru>
2020-03-07 19:41:46 +0300

    реализованы классы для разных типов заданий

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import Test from "./classes/Test";
-import Task from "./classes/Task";
+import { Task } from "./classes/Task";
 import Service from "./classes/Service";
 
 window.addEventListener("load", () => {
     const app = new Test();
 });
+
+// const taskData = [
+//     { type: "translate", question: "кот", answer: "cat" },
+//     { type: "phrase", question: "I have a cat" },
+//     { type: "word", question: "apple" },
+// ];
+//
+// const tasks = taskData.map(Task.create);
+// window.tasks = tasks;
+
+// window.currentTask = new TranslateTask({ question: "кот", answer: "cat" });
